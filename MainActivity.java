e5a4382cbbee59bea79a0e38dfd8aa800e305cd3
DRIFTYY777 <93861763+DRIFTYY777@users.noreply.github.com>
2022-04-03 14:11:19 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.gps;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Message;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textView;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        get_location_permission();
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        last_loc();
+    }
+
+
+
+    private void get_location_permission(){
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Precise location access granted.
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                            } else {
+                                // No location access granted.
+                            }
+                        }
+                );
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void last_loc(){
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                         double longitude = location.getLongitude();
+                         double latitude = location.getLatitude();
+
+                         textView = (TextView) findViewById(R.id.textView);
+
+                         String finalresult = new Double(longitude).toString();
+                         String finalresul = new Double(latitude).toString();
+
+                         textView.setText(finalresult  + "   " +  finalresul);
+
+
+
+                    }
+                });
+    }
+
+}
